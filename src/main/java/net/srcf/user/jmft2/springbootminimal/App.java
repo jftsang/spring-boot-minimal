5bcf3fcb80f4da98653c76f65f4303042e8c6d3b
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2023-08-19 18:52:11 +0100

    HTML and error examples

--- src/main/java/net/srcf/user/jmft2/springbootminimal/App.java
+++ src/main/java/net/srcf/user/jmft2/springbootminimal/App.java
@@ -2,6 +2,7 @@ package net.srcf.user.jmft2.springbootminimal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class App {
---
+++ src/main/java/net/srcf/user/jmft2/springbootminimal/HtmlController.java
@@ -0,0 +1,14 @@
+package net.srcf.user.jmft2.springbootminimal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HtmlController {
+
+  @GetMapping("/html")
+  public String htmlPage() {
+    return "index"; // This maps to the "index.html" template in the "templates" directory
+  }
+}
